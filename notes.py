9d3e93838b000516e2542769f2c76ea9b283c659
aranganath <ranganath2@llnl.gov>
2025-06-03 14:53:35 -0700

    removing unwanted files

--- notes.py
+++
@@ -1,5 +0,0 @@
-# Create the esm3 embeddings of pdbbind2020 sequence`s`
-# Create the denv2 2fom embeddings for 2fom sequence
-# Rank the distances and take first few example of th sequences.
-# Based on the selected values, rank based on the tanimoto similarity of the ligands.
-# Plot them on the graph (scatter).
